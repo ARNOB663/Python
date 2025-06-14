42e1c0cb2cc9b0b587ffc3389dc337c96588742b
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-06-14 11:09:49 +0600

    Create set.py

---
+++ set.py
@@ -0,0 +1,15 @@
+#set
+set1 = {1,2,2,2,2,2,3}
+print(set1)
+set2 = {1,2,3,0,1}
+set3 = set1.union(set2)
+print(set3)
+
+set4= set1.intersection(set2)
+print(set4)
+#Tuple
+tuple1 = (1,2,3,4,5)
+print(tuple)
+
+tuple.count(1)
+print(tuple)
