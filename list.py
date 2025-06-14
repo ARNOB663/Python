4f5a6183aa3cb6846dbff34e489e587ab9b0213f
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-06-14 09:59:50 +0600

    list

---
+++ list.py
@@ -0,0 +1,19 @@
+#list
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+#list can have a different type of data in the array
+fruits1 = ["apple",65,"test"]
+print(fruits1)
+#Regular indexing in a list
+fruits2 = ["apple","banana","cherry"]
+print(fruits2[0])
+#Replacing value in index
+fruits[0] = "orange"
+print(fruits)
+#negetive indexing
+print(fruits[-1])
+fruits[-1] = "mango"
+print(fruits)
+## list `append` function"
+fruits.append("apple")
+print(fruits)
--- test.py
+++ test.py
@@ -1,1 +1,28 @@
-print("Hello, world!")   
+# print("test")
+# i = 0
+# while i<10:
+#  print(i)
+#  i+=1
+
+ #A
+ #A B
+ #A B C
+
+# row = 1
+# while row <= 3:
+#     col = 0
+#     line=""
+#     while col < row:
+#         line += chr(ord('A') + col) + " "
+#         col += 1
+#     print(line)
+#     row += 1
+
+s = "python"
+for i in s: 
+    if i=="y":
+     continue
+
+    if i=='o':
+       break
+    print(i)
